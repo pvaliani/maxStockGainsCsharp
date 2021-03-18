4171104ce24c045ef10ad398d800d1f4a436db67
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-03-18 19:25:25 +0000

    returning both data sets as list of floats via console

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;  
+using System.Linq;
 
 namespace computershare
 {
@@ -7,19 +9,30 @@ namespace computershare
     {
         static void Main(string[] args)
         {
-    
-           
+
             Console.WriteLine("    ");
 
             // reads the first dataset as a single string which can be deserialized
             string dataSet1 = System.IO.File.ReadAllText(@"ChallengeSampleDataSet1.txt");
-            System.Console.WriteLine("ChallengeSampleDataSet1 values= {0}", dataSet1);
+            // System.Console.WriteLine("ChallengeSampleDataSet1 values= {0}", dataSet1);
 
             Console.WriteLine("    ");
 
             // reads the second dataset as a single string which can be deserialized
             string dataSet2 = System.IO.File.ReadAllText(@"ChallengeSampleDataSet2.txt");
-            System.Console.WriteLine("ChallengeSampleDataSet2 values= {0}", dataSet2);
+            // System.Console.WriteLine("ChallengeSampleDataSet2 values= {0}", dataSet2);
+
+            // convert dataSet1 string to a list of float values and send each value to the console
+            List<float> dataSet1asList = dataSet1.Split(',').Select(float.Parse).ToList();
+            dataSet1asList.ForEach(Console.WriteLine);
+
+            Console.WriteLine("  ----- THIS IS THE END OF DATASET 1 -----  ");
+
+            // convert dataSet1 string to a list of float values and send each value to the console
+            List<float> dataSet2asList = dataSet2.Split(',').Select(float.Parse).ToList();
+            dataSet2asList.ForEach(Console.WriteLine);
+
+            Console.WriteLine("  ----- THIS IS THE END OF DATASET 2 -----  ");
 
 
 
--- classes/StockData.cs
+++ classes/StockData.cs
@@ -2,6 +2,6 @@ namespace computershare.classes
 {
     public class StockData
     {
-        
+        // import the stock data here and save as two arrays containing the prices
     }
 }
